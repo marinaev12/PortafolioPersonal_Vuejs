bc634ca49604e2a55ce7d64364dce44a2a279a17
Marina Evangelista <marinaevangelista08@gmail.com>
2024-11-08 11:49:12 -0300

    Clases 9 y 10

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@ import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/EducacionPort.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
 import ProyectosComponente from './components/ProyectosComponente.vue';
+import HabilidadesComponente from './components/HabilidadesComponente.vue';
+import InteresesComponente from './components/InteresesComponente.vue';
 </script>
 
 <template>
-    <header>
+    <header id="top">
         <!--Barra de Navegación-->
          <NavBar />
          <!--Componente Datos Personales-->
          <DatosPersonales/>
-       
     </header>
 
     <main>
@@ -23,11 +24,33 @@ import ProyectosComponente from './components/ProyectosComponente.vue';
         <section id="proyectos"><h2>Proyectos</h2></section>
         <ProyectosComponente/>
         <section id="habilidades"><h2>Habilidades</h2></section>
-        <section id="intereses"><h2>Intereses</h2></section> 
+        <HabilidadesComponente/>
+        <section id="intereses"><h2>Intereses</h2></section>
+        <InteresesComponente/>
     </main>
 
+<footer>
+    <a href="#top">Inicio</a>
+    <p>© 2024 Marina Evangelista - Portafolio Web - Tutorial UTN FRSR</p>
+</footer>
 </template>
 
 <style scoped>
+footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    font-size: 1.3rem;
+    font-size: small;
+}
 
+footer a[href="#top"] {
+    color:  rgba(30, 30, 30);
+    text-decoration: none;
+}
+
+footer a[href="#top"]:hover {
+    color: darkblue; /* color al pasar el mouse */
+}
 </style>
---
+++ src/components/InteresesComponente.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros desarrolladores para mejorar herramientas y librerías populares.',
+    'Idiomas y Nuevas Culturas: Tengo una pasión por aprender nuevos idiomas y explorar diferentes culturas, lo cual enriquece mi perspectiva y comprensión del mundo.',
+    'Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia artificial y el desarrollo de aplicaciones móviles.',
+    'Yoga: Practico yoga como un hobby, lo que me ayuda a mantener un equilibrio físico y mental en mi vida profesional y personal.'
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+</style>
